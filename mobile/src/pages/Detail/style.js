a265289ca296b6f093321f5094239c5fea6be129
anderson-andr <andersontito15@gmail.com>
2020-04-29 03:19:21 -0300

    Mobile

--- mobile/src/pages/Detail/style.js
+++ mobile/src/pages/Detail/style.js
@@ -17,7 +17,7 @@ export default StyleSheet.create({
         borderRadius:8,
         backgroundColor:'#FFF',
         marginBottom:16,
-        marginTop:48,
+        marginTop:49,
     },
     incidentProperty:{
         fontSize:14,
